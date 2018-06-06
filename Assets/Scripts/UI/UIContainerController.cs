d4e6931423296112b6c13d521bff2ca323628d91
Sava Minic <savam@nordeus.com>
2018-06-07 00:21:20 +0200

    top fix for iphonex

---
+++ Assets/Scripts/UI/UIContainerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIContainerController : MonoBehaviour
+{
+	[SerializeField]
+	private float iphoneXOffset = 150f;
+
+	private RectTransform myRectTransform;
+
+	void Start()
+	{
+		myRectTransform = GetComponent<RectTransform>();
+		
+#if UNITY_IOS
+		if (UnityEngine.iOS.Device.generation == UnityEngine.iOS.DeviceGeneration.iPhoneX)
+		{
+			myRectTransform.offsetMax = new Vector2(myRectTransform.offsetMax.x, -iphoneXOffset);
+		}
+#endif
+		
+	}
+}
